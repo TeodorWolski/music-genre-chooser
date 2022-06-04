ee3644334806b788f5a600337fd40ff4397a4c69
TeodorWolski <teodorwolski88@gmail.com>
2022-06-04 14:16:45 +0200

    Styling the input layout and button

--- src/components/organisms/Combobox/Combobox.tsx
+++ src/components/organisms/Combobox/Combobox.tsx
@@ -1,12 +1,36 @@
 import styled from 'styled-components';
 
-const Input = styled.input``;
+const Wrapper = styled.div`
+  width: 40vw;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+const Input = styled.input`
+  padding: 1rem;
+  width: 25rem;
+  border-radius: 3rem;
+  font-size: ${({ theme }) => theme.fontSize.l};
+  border: 3px solid ${({ theme }) => theme.colors.green};
+  outline: none;
+`;
+
+const Button = styled.button`
+  background-color: ${({ theme }) => theme.colors.green};
+  border: none;
+  border-radius: 1.25rem;
+  color: white;
+  padding: 1rem 1.35rem;
+  font-size: ${({ theme }) => theme.fontSize.l};
+`;
 
 const Combobox = () => {
   return (
-    <div>
-      <Input />
-    </div>
+    <Wrapper>
+      <Input placeholder="Genre name" />
+      <Button>↓</Button>
+    </Wrapper>
   );
 };
 
