df6bea82600008c2635d4487d823e5461bb59adc
TeodorWolski <teodorwolski88@gmail.com>
2022-06-04 13:58:54 +0200

    Adding icons and creating mock data

---
+++ src/mocks/data/index.ts
@@ -0,0 +1,42 @@
+export const data = [
+  {
+    id: 1,
+    genre: 'Metal',
+    artist: 'Metallica',
+  },
+  {
+    id: 2,
+    genre: 'Rock',
+    artist: 'The Rolling Stones',
+  },
+  {
+    id: 3,
+    genre: 'Blues',
+    artist: 'Stevie Ray Vaughan',
+  },
+  {
+    id: 4,
+    genre: 'Disco',
+    artist: 'Stevie Wonder',
+  },
+  {
+    id: 5,
+    genre: 'Rap',
+    artist: 'Dr. Dre',
+  },
+  {
+    id: 6,
+    genre: 'Pop',
+    artist: 'Timbaland',
+  },
+  {
+    id: 7,
+    genre: 'Grunge',
+    artist: 'Nirvana',
+  },
+  {
+    id: 8,
+    genre: 'Classic',
+    artist: 'Johann Strauss',
+  },
+];
---
+++ src/types/types.ts
@@ -0,0 +1,5 @@
+export interface Genre {
+  id: number;
+  genre: string;
+  artist: string;
+}
