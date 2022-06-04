8fe9321b3213ef6d233080748846fa99e496c936
TeodorWolski <teodorwolski88@gmail.com>
2022-06-04 14:43:40 +0200

    Handle change visiblity

--- src/components/organisms/Combobox/Combobox.styles.ts
+++ src/components/organisms/Combobox/Combobox.styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
+  position: relative;
   width: 40vw;
   display: flex;
   align-items: center;
--- src/components/organisms/Combobox/Combobox.tsx
+++ src/components/organisms/Combobox/Combobox.tsx
@@ -1,10 +1,18 @@
+import { useState } from 'react';
 import { Wrapper } from './Combobox.styles';
+import GenresList from 'components/molecules/GenresList/GenresList';
 
 const Combobox = () => {
+  const [isVisible, setVisibility] = useState<boolean>(false);
+  const handleChangeVisibility = () => {
+    setVisibility(!isVisible);
+  };
+
   return (
     <Wrapper>
       <input placeholder="Genre name" />
-      <button>↓</button>
+      <button onClick={handleChangeVisibility}>↓</button>
+      <GenresList isVisible={isVisible} />
     </Wrapper>
   );
 };
