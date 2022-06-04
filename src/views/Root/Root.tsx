09aa7e9fa50e49ae2911361b3fad982cb7446e66
TeodorWolski <teodorwolski88@gmail.com>
2022-06-04 14:33:39 +0200

    Sending get request works

--- src/views/Root/Root.tsx
+++ src/views/Root/Root.tsx
@@ -1,8 +1,14 @@
+import { useEffect } from 'react';
 import MainTemplate from 'components/templates/MainTemplate';
 import { Heading } from './Root.styles';
 import Combobox from 'components/organisms/Combobox/Combobox';
+import axios from 'axios';
 
 const Root = () => {
+  useEffect(() => {
+    axios.get('/genres').then(({ data }) => console.log(data.Genres));
+  }, []);
+
   return (
     <MainTemplate>
       <Heading>Choose your favourite music</Heading>
