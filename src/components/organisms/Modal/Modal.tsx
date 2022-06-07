9dc9c564cda87ed03c636a4768cbf7bd14ac65c3
TeodorWolski <teodorwolski88@gmail.com>
2022-06-07 23:39:46 +0200

    Refs for inputs in modal

--- src/components/organisms/Modal/Modal.tsx
+++ src/components/organisms/Modal/Modal.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { useState, useRef } from 'react';
+import { Genre } from 'types/types';
 import { ModalWrapper, InputsWrapper, Wrapper } from './Modal.styles';
 
 interface ModalInterface {
@@ -7,6 +8,12 @@ interface ModalInterface {
 }
 
 const Modal: React.FC<ModalInterface> = ({ handleClose, isOpen }) => {
+  const [newGenre, setNewGenre] = useState<Genre>();
+  const nameRef = useRef<HTMLInputElement>(null);
+  const artistRef = useRef<HTMLInputElement>(null);
+  const spotifyRef = useRef<HTMLInputElement>(null);
+  const photoRef = useRef<HTMLInputElement>(null);
+
   return (
     <ModalWrapper
       ariaHideApp={false}
@@ -16,10 +23,10 @@ const Modal: React.FC<ModalInterface> = ({ handleClose, isOpen }) => {
       <Wrapper>
         <h1>Add new </h1>
         <InputsWrapper>
-          <input type="text" placeholder="Genre name" />
-          <input type="text" placeholder="Artist example" />
-          <input type="text" placeholder="Spotify url" />
-          <input type="file" placeholder="Picture" />
+          <input ref={nameRef} type="text" placeholder="Genre name" />
+          <input ref={artistRef} type="text" placeholder="Artist example" />
+          <input ref={spotifyRef} type="text" placeholder="Spotify url" />
+          <input ref={photoRef} type="file" placeholder="Picture" />
         </InputsWrapper>
         <div>
           <button>Add new genre!</button>
