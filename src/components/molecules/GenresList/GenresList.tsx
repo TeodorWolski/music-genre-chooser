9600f6eab79e10aba0df22e3abef5e7d74152e99
TeodorWolski <teodorwolski88@gmail.com>
2022-06-07 21:04:01 +0200

    Searching genre

--- src/components/molecules/GenresList/GenresList.tsx
+++ src/components/molecules/GenresList/GenresList.tsx
@@ -1,25 +1,21 @@
-import { useEffect, useContext } from 'react';
+import { useContext } from 'react';
 import { Wrapper } from './GenresList.styles';
-import { useSelector, useDispatch } from 'react-redux';
-import { RootState } from 'redux/store/store';
-import Genre from 'components/atoms/Genre/Genre';
 import { SelectContext } from 'providers/SelectProvider';
-import { ContextProps } from 'types/types';
+import { ContextProps, Genre as GenreProps } from 'types/types';
+import Genre from 'components/atoms/Genre/Genre';
 
-const GenresList: React.FC<{ isVisible: boolean }> = ({ isVisible }) => {
-  const dispatch = useDispatch();
-  const genres = useSelector((state: RootState) => state);
+interface Props {
+  genresList: GenreProps[];
+  isVisible: boolean;
+}
+
+const GenresList: React.FC<Props> = ({ isVisible, genresList }) => {
   const { addGenre } = useContext<ContextProps>(SelectContext);
-  const fetchGenres = () => ({ type: 'GENRES_FETCH_REQUESTED' });
-
-  useEffect(() => {
-    dispatch(fetchGenres());
-  }, []);
 
   return (
     <>
       <Wrapper isVisible={isVisible}>
-        {genres.map(({ image, genre, id, artist, url }) => (
+        {genresList.map(({ image, genre, id, artist, url }) => (
           <Genre
             url={url}
             key={id}
--- src/components/organisms/Combobox/Combobox.tsx
+++ src/components/organisms/Combobox/Combobox.tsx
@@ -1,23 +1,59 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from 'redux/store/store';
+import { Genre } from 'types/types';
 import { Wrapper, Button } from './Combobox.styles';
 import SelectedGenresList from 'components/molecules/SelectedGenresList/SelectedGenresList';
 import GenresList from 'components/molecules/GenresList/GenresList';
 
 const Combobox = () => {
+  const [searchTerm, setSearchTerm] = useState('');
   const [isVisible, setVisibility] = useState<boolean>(false);
-  const handleChangeVisibility = () => {
-    setVisibility(!isVisible);
+  const [searchedItem, setSearchedItem] = useState<Genre[]>([]);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const dispatch = useDispatch();
+  const fulledGenresList = useSelector((state: RootState) => state);
+  const fetchGenres = () => ({ type: 'GENRES_FETCH_REQUESTED' });
+
+  useEffect(() => {
+    dispatch(fetchGenres());
+  }, []);
+
+  const handleInputChange = () => {
+    setVisibility(true);
+
+    if (inputRef.current) {
+      setSearchTerm(inputRef.current.value);
+    }
+
+    const searchedGenre = fulledGenresList.filter((item) => {
+      const searchedTerm = searchTerm.toLowerCase();
+      const loweredGenre = item.genre.toLowerCase();
+
+      return loweredGenre.startsWith(searchedTerm);
+    });
+
+    setSearchedItem(searchedGenre);
   };
 
   return (
     <>
       <SelectedGenresList />
       <Wrapper>
-        <input placeholder="Genre name" />
-        <Button onClick={handleChangeVisibility}>
+        <input
+          ref={inputRef}
+          onChange={handleInputChange}
+          placeholder="Genre name"
+        />
+        <Button onClick={() => setVisibility(!isVisible)}>
           {isVisible ? '↑' : '↓'}
         </Button>
-        <GenresList isVisible={isVisible} />
+        {searchTerm === '' ? (
+          <GenresList genresList={fulledGenresList} isVisible={isVisible} />
+        ) : (
+          <GenresList genresList={searchedItem} isVisible={isVisible} />
+        )}
       </Wrapper>
     </>
   );
