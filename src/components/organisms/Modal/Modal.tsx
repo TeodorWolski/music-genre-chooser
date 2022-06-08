241e61137685e31268bd6e90079241aa079ae4db
TeodorWolski <teodorwolski88@gmail.com>
2022-06-08 08:37:38 +0200

    creating logic for adding new genre

--- src/components/organisms/Modal/Modal.tsx
+++ src/components/organisms/Modal/Modal.tsx
@@ -1,5 +1,4 @@
 import { useState, useRef } from 'react';
-import { Genre } from 'types/types';
 import { ModalWrapper, InputsWrapper, Wrapper } from './Modal.styles';
 
 interface ModalInterface {
@@ -8,11 +7,33 @@ interface ModalInterface {
 }
 
 const Modal: React.FC<ModalInterface> = ({ handleClose, isOpen }) => {
-  const [newGenre, setNewGenre] = useState<Genre>();
+  const [newGenre, setNewGenre] = useState<null | object>({});
+  const id = Math.floor(Math.random() * 100);
   const nameRef = useRef<HTMLInputElement>(null);
   const artistRef = useRef<HTMLInputElement>(null);
   const spotifyRef = useRef<HTMLInputElement>(null);
   const photoRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (
+      nameRef.current ||
+      artistRef.current ||
+      spotifyRef.current ||
+      photoRef.current
+    ) {
+      setNewGenre({
+        id: id,
+        genre: nameRef.current?.value,
+        artist: artistRef.current?.value,
+        url: spotifyRef.current?.value,
+        image: photoRef.current?.value,
+      });
+    }
+
+    console.log(newGenre);
+  };
 
   return (
     <ModalWrapper
@@ -20,7 +41,7 @@ const Modal: React.FC<ModalInterface> = ({ handleClose, isOpen }) => {
       isOpen={isOpen}
       onRequestClose={handleClose}
     >
-      <Wrapper>
+      <Wrapper onSubmit={handleSubmit}>
         <h1>Add new </h1>
         <InputsWrapper>
           <input ref={nameRef} type="text" placeholder="Genre name" />
@@ -29,7 +50,7 @@ const Modal: React.FC<ModalInterface> = ({ handleClose, isOpen }) => {
           <input ref={photoRef} type="file" placeholder="Picture" />
         </InputsWrapper>
         <div>
-          <button>Add new genre!</button>
+          <button type="submit">Add new genre!</button>
           <button style={{ backgroundColor: 'red' }} onClick={handleClose}>
             Close
           </button>
