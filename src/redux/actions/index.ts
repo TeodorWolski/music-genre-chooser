b078d6989bfc0c00522bba72303f7cdd7daf7794
TeodorWolski <teodorwolski88@gmail.com>
2022-06-08 09:01:40 +0200

    addGenre action

--- src/redux/actions/index.ts
+++ src/redux/actions/index.ts
@@ -1,1 +1,8 @@
+import { Genre } from 'types/types';
+
 export const fetchGenres = () => ({ type: 'GENRES_FETCH_REQUESTED' });
+
+export const addGenre = ({ id, genre, url, image, artist }: Genre) => ({
+  type: 'CREATE_GENRE_REQUESTED',
+  payload: { id, genre, url, image, artist },
+});
